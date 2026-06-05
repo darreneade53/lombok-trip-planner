d74c3f9116ed211c58aa242c5bf087201eed7580
darreneade53 <darreneade53@gmail.com>
2026-06-04 20:05:33 -0400

    Update generate.js

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -5,7 +5,7 @@ exports.handler = async function(event) {
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'POST, OPTIONS'
+        'Access-Control-Allow-Methods': 'POST, OPTIONS' 
       },
       body: ''
     };
